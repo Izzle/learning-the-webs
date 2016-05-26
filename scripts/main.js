8644c08048ff91f021c14cbd1ba88b41a9fbbca0
Ron Martin <ronmartin89@gmail.com>
2016-05-26 01:35:13 -0700

    Added main.js

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+/* 
+	This is a block comment
+*/
+
+// this is a line comment
+var iceCream = 'chocolate';
+if (iceCream === 'chocolate') {
+	alert('Yay, I love chocolate ice cream!');
+} else {
+	alert('Awww, but chocolate is my favorite...');
+}
