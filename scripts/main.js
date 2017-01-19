adb6a3c44c06f02f1b43b6a9d0540675f71f1780
Ron Martin <ronmartin89@gmail.com>
2017-01-19 03:06:11 -0800

    building the countdown clock til 2018 in javascript

--- scripts/main.js
+++ scripts/main.js
@@ -14,4 +14,24 @@ myImage.onclick = function() {
 	}
 }
 
+var timeTil = function() {
+	//JAVASCRIPT TIME CALCULATOR: how much time is it in milliseconds from 1970 from your specified date?
+
+var milSec = 1000;
+var tMin = 60;
+var tHour = 60;
+var tDay = 24;
+var fDay = milSec * tMin * tHour * tDay;
+
+var leapYearZero = 1972;  //the first leap year since 1970
+// MAKE A COUNTER HERE SO WE CAN CREATE THE APPROPRIATE TIME ie(365,365,365,366,365,365,etc.)
+var tLeapYear = 366 * fDay;
+var tYear = 365 * fDay;
+var tTravel = 2017 - 1970;
+document.getElementById("PLACEHOLDER1").innerHTML = tYear * tTravel;
+document.getElementById("PLACEHOLDER2").innerHTML = display;
+};
+
+document.getElementById("countDown").innerHTML = Date();
+
 console.log("test");
